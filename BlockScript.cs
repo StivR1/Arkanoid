2e47ae21117567894108b2196d5a927e47bd0855
StivR1 <39085952+StivR1@users.noreply.github.com>
2018-05-21 19:08:56 +0500

    Add files via upload

---
+++ BlockScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockScript : MonoBehaviour
+{
+
+    public int hitsToKill;
+    public int points;
+    private int numberOfHits;
+
+    // используйте этот метод для инициализации
+    void Start()
+    {
+        numberOfHits = 0;
+    }
+
+    // Update вызывается при отрисовке каждого кадра игры
+    void Update()
+    {
+
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ball")
+        {
+            numberOfHits++;
+
+            if (numberOfHits == hitsToKill)
+            {
+                // уничтожаем объект
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
