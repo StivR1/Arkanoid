08bfb36d9c45a4f86b691b395d715817d792bedc
StivR1 <39085952+StivR1@users.noreply.github.com>
2018-05-21 16:37:24 +0500

    Add files via upload

---
+++ BallScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallScript : MonoBehaviour
+{
+    private new Rigidbody rigidbody2D;
+    private bool ballIsActive;
+    private Vector3 ballPosition;
+    private Vector2 ballInitialForce;
+
+    // GameObject
+    public GameObject playerObject;
+
+    // используйте этот метод для инициализации
+    void Start()
+    {
+        // создаем силу
+        ballInitialForce = new Vector2(100.0f, 300.0f);
+
+        // переводим в неактивное состояние
+        ballIsActive = false;
+
+        // запоминаем положение
+        ballPosition = transform.position;
+    }
+
+    void Update()
+    {
+        // проверка нажатия на пробел
+        if (Input.GetButtonDown("Jump") == true)
+        {
+            // проверка состояния
+            if (!ballIsActive)
+            {
+                // сброс всех сил
+                rigidbody2D.isKinematic = false;
+                // применим силу
+                rigidbody2D.AddForce(ballInitialForce);
+                // зададим активное состояние
+                ballIsActive = !ballIsActive;
+            }
+
+            if (!ballIsActive && playerObject != null)
+            {
+                // задаем новую позицию шарика
+                ballPosition.x = playerObject.transform.position.x;
+
+                // устанавливаем позицию шара
+                transform.position = ballPosition;
+            }
+
+            // проверка падения шара
+            if (ballIsActive && transform.position.y < -6)
+            {
+                ballIsActive = !ballIsActive;
+                ballPosition.x = playerObject.transform.position.x;
+                ballPosition.y = -4.2f;
+                transform.position = ballPosition;
+
+                rigidbody2D.isKinematic = true;
+            }
+        }
+    }
+}
---
+++ PlayerScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float boundary;
+    public float playerVelocity;
+    private Vector3 playerPosition;
+
+    // используйте этот метод для инициализации
+    void Start()
+    {
+        // получим начальную позицию платформы
+        playerPosition = gameObject.transform.position;
+    }
+
+    // Update вызывается при отрисовке каждого кадра игры
+    void Update()
+    {
+        // горизонтальное движение
+        playerPosition.x += Input.GetAxis("Horizontal") * playerVelocity;
+
+        // выход из игры
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        // обновим позицию платформы
+        transform.position = playerPosition;
+
+        // проверка выхода за границы
+        if (playerPosition.x < -boundary)
+        {
+            transform.position = new Vector3(-boundary, playerPosition.y, playerPosition.z);
+        }
+        if (playerPosition.x > boundary)
+        {
+            transform.position = new Vector3(boundary, playerPosition.y, playerPosition.z);
+        }
+    }
+}
